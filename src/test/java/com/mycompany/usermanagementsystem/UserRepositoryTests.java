7dc6fc4f4c716f125426903c6650c385aa74a55b
jordanfoote <jordan.foote@outlook.com>
2022-08-23 21:47:57 -0500

    creating user entity

---
+++ src/test/java/com/mycompany/usermanagementsystem/UserRepositoryTests.java
@@ -0,0 +1,31 @@
+package com.mycompany.usermanagementsystem;
+
+import com.mycompany.usermanagementsystem.user.User;
+import com.mycompany.usermanagementsystem.user.UserRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Rollback;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Rollback(false)
+public class UserRepositoryTests {
+    @Autowired private UserRepository repo;
+
+    @Test
+    public void testAddNew() {
+        User user = new User();
+        user.setEmail("bobby.john@gmail.com");
+        user.setPassword("bobby12345");
+        user.setFirstName("Bobby");
+        user.setLastName("John");
+
+        User savedUser = repo.save(user);
+
+        Assertions.assertThat(savedUser).isNotNull();
+        Assertions.assertThat(savedUser.getId()).isGreaterThan(0);
+    }
+}
